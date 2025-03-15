c00bee2a716f70abd58537360fd31c49f29e2378
Frederico Hollerbach <frederico.hollerbach@gmail.com>
2025-03-15 01:20:19 -0300

    Acerto Rota e Tela Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redireciona para login por padrão
+  { path: '**', redirectTo: 'home' }                   // Redireciona para login caso a rota não exista
+];
