fd6e15d7dad51e392486e3302b8220f309bfdde6
Frederico Hollerbach <frederico.hollerbach@gmail.com>
2025-03-15 20:10:47 -0300

    Header apartado e tratado para comportamento em home e login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Input() showMenus: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Se desejar manter alguma lógica de verificação de rota, ajuste-a conforme necessário.
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Opcional: atualizar a propriedade com base na rota
+        // this.showMenus = event.url === '/home';
+      }
+    });
+  }
 }
