c8c9616f38acb9282f10f0bc3e2bacd97461ec45
Frederico Hollerbach <frederico.hollerbach@gmail.com>
2025-03-28 22:31:21 -0300

    Show/Hide Password

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  showPassword = false;
 
+  toggleShowPassword(): void {
+    this.showPassword = !this.showPassword;
+  }
 }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+  showPassword = false;
 
+  toggleShowPassword(): void {
+    this.showPassword = !this.showPassword;
+  }
 }
